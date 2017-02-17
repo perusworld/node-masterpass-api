a731b199f2e5261396b0395e764fa48f8d458f90
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2017-02-16 16:03:54 -0800

    simple name

--- README.md
+++ README.md
@@ -16,13 +16,13 @@ const fs = require('fs');
 var masterpassapi = require('node-masterpass-api').masterpass();
 var private = fs.readFileSync("privateKey", "utf8");
 
-var masterpassOAuth = new masterpassapi.Masterpass({
+var masterpass = new masterpassapi.Masterpass({
     privateKey: private,
     consumerKey: '------your consumer key ---',
     callBackUrl: 'https://www.blah.com/blah'
 });
 
-masterpassOAuth.requestToken((err, resp) => {
+masterpass.requestToken((err, resp) => {
     if (err) {
         console.log(err);
     } else {
--- test.js
+++ test.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 var masterpassapi = require('./node-masterpass-api').masterpass();
 var private = fs.readFileSync("privateKey", "utf8");
 
-var masterpassOAuth = new masterpassapi.Masterpass({
+var masterpass = new masterpassapi.Masterpass({
     privateKey: private,
     consumerKey: '------your consumer key ---',
     callBackUrl: 'https://www.blah.com/blah'
 });
 
-masterpassOAuth.requestToken((err, resp) => {
+masterpass.requestToken((err, resp) => {
     if (err) {
         console.log(err);
     } else {
